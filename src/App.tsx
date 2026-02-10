0f5028001505853f89e340c80a87b4015af223ac
Matthew Wolff <mtwol@amazon.com>
2026-02-10 09:26:44 -0800

    refactor: Add subpage index.html files with hash routing for trailing slash support

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Valentine from './pages/Valentine';
 import Yes from './pages/Yes';
 import No from './pages/No';
 
+const AppRoutes: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Handle 404 redirects with hash
+    const hash = window.location.hash;
+    if (hash && hash.startsWith('#/')) {
+      const path = hash.replace('#', '');
+      navigate(path, { replace: true });
+    }
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/valentine/" replace />} />
+      <Route path="/valentine/" element={<Valentine />} />
+      <Route path="/valentine/yes/" element={<Yes />} />
+      <Route path="/valentine/no/" element={<No />} />
+    </Routes>
+  );
+};
+
 const App: React.FC = () => {
   return (
     <BrowserRouter basename="/gerbus">
-      <Routes>
-        <Route path="/" element={<Navigate to="/valentine" replace />} />
-        <Route path="/valentine" element={<Valentine />} />
-        <Route path="/valentine/yes" element={<Yes />} />
-        <Route path="/valentine/no" element={<No />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 };
